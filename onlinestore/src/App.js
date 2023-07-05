a19203e02a70f3cca4adbf181f20dd7dac9f51a4
DnD07 <07dnd.dahal@gmail.com>
2023-07-05 07:34:41 -0500

    added Home.js

--- onlinestore/src/App.js
+++ onlinestore/src/App.js
@@ -1,12 +1,26 @@
-import react from 'react'
+import React from 'react'
+// import {Helmet} from "react-helmet";
+import Home from './Home';
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
 
 const App = () =>{
     return(
         <>
-        hvbf
-        cjndjbdsc
-        jncdkdjcb
-        jdcsjdc
+        <div className='app'>
+        {/* <Helmet>
+                <meta charSet="utf-8" />
+                <title>Online Store</title>
+                <link rel="canonical" href="http://mysite.com/example" />
+                <meta name="description" content="Site icon "/>
+            </Helmet> */}
+
+       <BrowserRouter>
+       <Routes>
+        <Route index element={<Home/>}/>
+       </Routes>
+       </BrowserRouter>
+        </div>
         </>
     );
 }
--- onlinestore/src/Home.js
+++ onlinestore/src/Home.js
@@ -1,2 +1,28 @@
-import react from "react" 
+import React from "react" 
 
+
+const Home = ()=>{
+    return(
+        <>
+        <header className="navbar">
+            <div className="container-xxl">
+                <div className="row">
+                    <div className="col-6">
+                        mcksd
+                        cjdscbjds
+
+                    </div>
+                    <div className="col-6">
+djcjd
+                    </div>
+
+                </div>
+
+            </div>
+
+        </header>
+        </>
+    );
+}
+
+export default Home;
